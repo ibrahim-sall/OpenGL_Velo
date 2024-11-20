08658bde663525f87df709bceb9ee17b00289bf7
ibrahim-sall <76476132+ibrahim-sall@users.noreply.github.com>
2024-11-20 09:08:50 +0100

    Delete build directory

--- build/CMakeFiles/Open_gl.dir/Open_gl_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Open_gl.dir/Open_gl_autogen/mocs_compilation.cpp.o: \
- /home/ibhou/Documents/OpenGL/OpenGL_API_V6/build/Open_gl_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Open_gl_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Open_gl_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
